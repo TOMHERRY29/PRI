d5f35a620763cdd1ab8ccbb159dda84a63683ed9
LASSIRI Yahya <yahya.lassiri.96@gmail.com>
2019-05-05 13:07:43 +0200

    Modification code procédure StagiaireAjoutOuModification (Adaptation serveur XAMPP MYSQL)

--- BackEnd/procedure_ajout_modif_stagiaire.sql
+++ BackEnd/procedure_ajout_modif_stagiaire.sql
@@ -1,15 +1,12 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `StagiaireAjoutOuModification`(
-IN _idStagiaire varchar(60),
-IN _Nom varchar(60),
-IN _Prenom varchar(60)
-)
-BEGIN
-	SELECT @A := COUNT(idStagiaire) FROM stagiaire WHERE idStagiaire = _idStagiaire;
-	IF @A = 0 THEN
-		INSERT INTO stagiaire(idStagiaire,Nom,Prenom)
-        values(_idStagiaire,_Nom,_Prenom);        
-	ELSE
-		UPDATE Stagiaire SET Nom = _Nom, Prenom = _Prenom WHERE idStagiaire = _idStagiaire;
-	END IF;
-    
-END
+CREATE PROCEDURE `StagiaireAjoutOuModification`(
+	IN `_idStagiaire` VARCHAR(60),
+	IN `_Nom` VARCHAR(60), 
+	IN `_Prenom` VARCHAR(60)) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
+	BEGIN 
+		SELECT @A := COUNT(idStagiaire) FROM stagiaire WHERE idStagiaire = _idStagiaire; 
+		IF @A = 0 THEN 
+			INSERT INTO stagiaire(idStagiaire,Nom,Prenom) values(_idStagiaire,_Nom,_Prenom); 
+		ELSE 
+			UPDATE Stagiaire SET Nom = _Nom, Prenom = _Prenom WHERE idStagiaire = _idStagiaire; 
+		END IF;
+	END
