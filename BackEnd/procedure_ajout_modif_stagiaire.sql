945841bb1fd71227207429bebc416b99f106687a
LASSIRI Yahya <yahya.lassiri.96@gmail.com>
2019-05-05 14:26:41 +0200

    correction code procédure StagiaireAjoutOuModification

--- BackEnd/procedure_ajout_modif_stagiaire.sql
+++ BackEnd/procedure_ajout_modif_stagiaire.sql
@@ -1,12 +1,13 @@
-CREATE PROCEDURE `StagiaireAjoutOuModification`(
-	IN `_idStagiaire` VARCHAR(60),
-	IN `_Nom` VARCHAR(60), 
-	IN `_Prenom` VARCHAR(60)) NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
-	BEGIN 
-		SELECT @A := COUNT(idStagiaire) FROM stagiaire WHERE idStagiaire = _idStagiaire; 
-		IF @A = 0 THEN 
-			INSERT INTO stagiaire(idStagiaire,Nom,Prenom) values(_idStagiaire,_Nom,_Prenom); 
-		ELSE 
-			UPDATE Stagiaire SET Nom = _Nom, Prenom = _Prenom WHERE idStagiaire = _idStagiaire; 
-		END IF;
-	END
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `StagiaireAjoutOuModification`(IN `_idStagiaire` VARCHAR(60), IN `_Nom` VARCHAR(60), IN `_Prenom` VARCHAR(60))
+    NO SQL
+BEGIN
+SELECT @A := COUNT(idStagiaire) FROM stagiaire WHERE idStagiaire = _idStagiaire;
+	IF @A = 0 THEN
+		INSERT INTO stagiaire(idStagiaire,Nom,Prenom)
+        values(_idStagiaire,_Nom,_Prenom);        
+	ELSE
+		UPDATE Stagiaire SET Nom = _Nom, Prenom = _Prenom WHERE idStagiaire = _idStagiaire;
+	END IF;
+END$$
+DELIMITER ;
