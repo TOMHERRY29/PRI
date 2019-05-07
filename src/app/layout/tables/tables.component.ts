51080845cc957063ffe021cf1f250a001c157b64
Hamza Rafaa <h6rafaa@enib.fr>
2019-05-07 16:55:31 +0200

    page de entreprise

--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -8,6 +8,34 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class TablesComponent implements OnInit {
+
+//Récupérer ici toutes les informations sur les stages
+entreprises = [
+
+    {
+      
+        nom: 'Sopra Steria',
+        pays: 'France',
+        ville: "Brest"
+
+    },
+    {
+      
+        nom: 'SFR',
+        pays: 'France',
+        ville: "Lyon"
+  
+      },
+      {
+      
+        nom: 'Capgemini',
+        pays: 'France',
+        ville: "Nantes"
+  
+      }
+      
+    ];
+
     constructor() {}
 
     ngOnInit() {}
